b7fdb9e8e2e2608d627b811f3297676263f8aace
Liam Ward <liamtgward@gmail.com>
2022-12-01 15:59:32 +1000

    Completed Chalenges 1 and 2

---
+++ AdventOfCode/Days/Day1.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode.Days
+{
+    internal class Day1
+    {
+        public const string fileloc = "C:\\Users\\Liam\\source\\repos\\AdventOfCode\\AdventOfCode\\PuzzleInputs\\Day1Input.txt";
+
+        private static List<int> GetTotalCalorieList()
+        {
+            string[] loadedtext = File.ReadAllLines(fileloc);
+            List<int> calorieList = new List<int>();
+
+            var runningtotal = 0;
+
+            foreach (var line in loadedtext)
+            {
+                if (line == string.Empty)
+                {
+                    calorieList.Add(runningtotal);
+                    runningtotal = 0;
+                    continue;
+                }
+                runningtotal = int.Parse(line) + runningtotal;
+            }
+
+            return calorieList;
+        }
+
+        public static int ReturnHighest()
+        {
+            return GetTotalCalorieList().Max();
+        }
+
+        public static int ReturnTopNthSummed(int n)
+        {
+            return GetTotalCalorieList().OrderByDescending(p => p).Take(n).Sum();
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode.Days;
+
+namespace AdventOfCode
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Day1.ReturnHighest());
+            Console.WriteLine(Day1.ReturnTopNthSummed(3));
+
+        }
+    }
+}
